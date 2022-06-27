4aac428e7e1bf04128523426c069c5ededd10f62
michaeleyitayo <archangelmichael516@gmail.com>
2022-06-27 18:05:01 +0100

    product restyle

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -6,9 +6,11 @@ const Product: React.FC<IProduct> = (product) => {
   const { setShowCart } = useCart();
   return (
     <div className="flex flex-col items-center">
-      <img src={product.image_url} alt="" className="w-full" />
+      <div className="h-[60px]">
+        <img src={product.image_url} className="h-full" alt="" />
+      </div>
       <h4 className="text-[13px] font-normal">{product.title}</h4>
-      <p className="text-[13px] font-normal">From {product.price}</p>
+      <p className="text-[13px] font-normal">From ${product.price}</p>
       <button
         className="bg-[#4b5548] text-[16px] text-white w-5/6 py-[10px] mt-[20px]"
         onClick={() => setShowCart(true)}
